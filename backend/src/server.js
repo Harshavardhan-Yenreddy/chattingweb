dae9b15f48255d3694fb8614788ab88ad67ec193
Harshavardhan-Yenreddy <hyenreddy@gmail.com>
2026-07-28 23:12:49 +0530

    signup added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
 dotenv.config();
 const app = express();
+const __dirname = path.resolve();
 const Port=process.env.PORT;
 
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
-app.listen(Port,()=> console.log(`server is running on port ${Port}`));
+//deployment
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend/dist/index.html"));
+    });
+}
+app.listen(Port,()=> {
+    console.log(`server is running on port ${Port}`)
+    connectDB();
+});
